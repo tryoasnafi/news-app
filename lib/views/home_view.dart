074d7bf2bf5f28a0c179c3a791c4b9be370c19b0
Tryo Asnafi <tryoasnafi@gmail.com>
2021-04-27 12:07:20 +0700

    add page views

---
+++ lib/views/home_view.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+import 'tabs/popular_tab_view.dart';
+import 'tabs/recent_tab_view.dart';
+import 'tabs/trending_tab_view.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(120.0),
+          child: Column(
+            children: [
+              ListTile(
+                title: Text(
+                  'WELCOME',
+                  textAlign: TextAlign.end,
+                  style: kNonActiveTabStyle,
+                ),
+                subtitle: Text(
+                  'Jessica Veranda',
+                  textAlign: TextAlign.end,
+                  style: kActiveTabStyle,
+                ),
+                trailing: Container(
+                  width: 50.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    image: DecorationImage(
+                      image: AssetImage('assets/ve.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: TabBar(
+                  labelColor: kBlack,
+                  labelStyle: kActiveTabStyle.copyWith(
+                    fontSize: 24.0,
+                  ),
+                  isScrollable: true,
+                  indicatorColor: Colors.white,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  unselectedLabelColor: kGrey1,
+                  unselectedLabelStyle: kNonActiveTabStyle.copyWith(
+                    fontSize: 16.0,
+                  ),
+                  tabs: [
+                    Tab(
+                      text: 'Popular',
+                    ),
+                    Tab(
+                      text: 'Trending',
+                    ),
+                    Tab(
+                      text: 'Recent',
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            PopularTabView(),
+            TrendingTabView(),
+            RecentTabView(),
+          ],
+        ),
+      ),
+    );
+  }
+}
