e9dfc7a5a02afefed615187f9b2f78e35683079a
Tryo Asnafi <tryoasnafi@gmail.com>
2021-05-19 17:05:58 +0700

    Update the datas fetched from api

--- lib/views/tabs/recent_tab_view.dart
+++ lib/views/tabs/recent_tab_view.dart
@@ -1,40 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/services/api_service.dart';
 import '../../models/news.dart';
 import '../../widgets/secondary_card.dart';
 import '../read_news_view.dart';
 
-class RecentTabView extends StatelessWidget {
+class RecentTabView extends StatefulWidget {
+  @override
+  _RecentTabViewState createState() => _RecentTabViewState();
+}
+
+class _RecentTabViewState extends State<RecentTabView> {
+  ApiService client = ApiService();
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: recentList.length,
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      physics: BouncingScrollPhysics(),
-      itemBuilder: (context, index) {
-        var _recentNews = recentList[index];
+    return FutureBuilder(
+      future: client.fetchNews(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<News> news = snapshot.data;
 
-        return InkWell(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => ReadNews(
-                  news: _recentNews,
+          return ListView.builder(
+            itemCount: news.length,
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            physics: BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              var _recentNews = news[index];
+
+              return InkWell(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ReadNews(
+                        news: _recentNews,
+                      ),
+                    ),
+                  );
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 135.0,
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 8.0,
+                  ),
+                  child: SecondaryCard(
+                    news: _recentNews,
+                  ),
                 ),
-              ),
-            );
-          },
-          child: Container(
-            width: double.infinity,
-            height: 135.0,
-            margin: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 8.0,
-            ),
-            child: SecondaryCard(
-              news: _recentNews,
-            ),
-          ),
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        return Center(
+          child: CircularProgressIndicator(),
         );
       },
     );
--- lib/views/tabs/trending_tab_view.dart
+++ lib/views/tabs/trending_tab_view.dart
@@ -1,38 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/services/api_service.dart';
 import '../../models/news.dart';
 import '../../widgets/primary_card.dart';
 import '../read_news_view.dart';
 
-class TrendingTabView extends StatelessWidget {
+class TrendingTabView extends StatefulWidget {
+  @override
+  _TrendingTabViewState createState() => _TrendingTabViewState();
+}
+
+class _TrendingTabViewState extends State<TrendingTabView> {
+  ApiService client = ApiService();
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: trendingList.length,
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      physics: BouncingScrollPhysics(),
-      itemBuilder: (context, index) {
-        var _trendingNews = trendingList[index];
+    return FutureBuilder(
+      future: client.fetchNews(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<News> news = snapshot.data;
 
-        return InkWell(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => ReadNews(
-                  news: _trendingNews,
+          return ListView.builder(
+            itemCount: news.length,
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            physics: BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              var _trendingNews = news[index];
+
+              return InkWell(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ReadNews(
+                        news: _trendingNews,
+                      ),
+                    ),
+                  );
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 300.0,
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 8.0,
+                  ),
+                  child: PrimaryCard(
+                    news: _trendingNews,
+                  ),
                 ),
-              ),
-            );
-          },
-          child: Container(
-            width: double.infinity,
-            height: 300.0,
-            margin: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 8.0,
-            ),
-            child: PrimaryCard(news: _trendingNews),
-          ),
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        return Center(
+          child: CircularProgressIndicator(),
         );
       },
     );
